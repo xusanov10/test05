74df8fad42eca20156902e833adfbbb79029fa65
xusanov10 <ahmadjonxusanovv>
2025-04-10 21:38:57 +0500

    change11

--- test5/Program.cs
+++ test5/Program.cs
@@ -9,4 +9,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
 
